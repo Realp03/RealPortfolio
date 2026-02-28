dd108120540e84925d2f6c389d2677602bbbf7fd
Realp03 <pinedamarkdaryl@gmail.com>
2026-02-28 15:04:44 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const AI_URL = "https://poison-continue-choices-humanitarian.trycloudflare.com";
+  const AI_URL = "https://postage-blvd-etc-sports.trycloudflare.com";
   const items = document.querySelectorAll(".reveal");
   items.forEach((el, i) => {
     el.style.setProperty("--delay", `${0.12 * i}s`);
@@ -345,4 +345,5 @@ if (g) {
     g.src = "gallereal.png";
   };
 }
+
 
